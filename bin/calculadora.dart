0477afd3cdf93e55763fa3326ca8bfc0f2e2e7a7
Anne Caroline Rocha <carolsprak@gmail.com>
2020-08-12 05:28:07 +0000

    assertivas

--- bin/calculadora.dart
+++ bin/calculadora.dart
@@ -5,4 +5,17 @@ class Calculadora {
   dynamic somar(dynamic a, dynamic b) {
     return a + b;
   }
+
+  int somaDados(int a, int b) {
+    try {
+      assert(a <= 6.0, a);
+      assert(b <= 6.0, b);
+
+      return somar(a, b);
+    } catch (e) {
+      print(e);
+    }
+    
+    return 0;
+  }
 }
--- main.dart
+++ main.dart
@@ -5,4 +5,5 @@ void main(){
 
   Calculadora c = Calculadora();
   print("Resultado: ${c.somar(1,2)}"); 
+  print("Resultado: ${c.somaDados(10,5)}"); 
 }
